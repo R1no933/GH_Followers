6e503d4a9365b5c15ce5f647e2c918c40526a8dc
R1no933 <104028198+R1no933@users.noreply.github.com>
2022-08-28 23:02:04 +0300

    user_info_screen
    -present modal screen
    -correct user info & follower model
    -complete network call for user info
    -add and configure parent view controller for github info and followers section
    -add and configure child view controller for github info and followers section
    -configure button
    -add delegate and action for button
    -add safari service

---
+++ GH_Followers/CustomViews/ViewControllers/GHFUserInfoHeaderViewController.swift
@@ -0,0 +1,100 @@
+//
+//  GHFUserInfoHeaderViewController.swift
+//  GH_Followers
+//
+//  Created by Dmitriy Baskakov on 28.08.2022.
+//
+
+import UIKit
+
+class GHFUserInfoHeaderViewController: UIViewController {
+    
+    //MARK: - Properties
+    let avatarImageView = GHFAvataImageView(frame: .zero)
+    let usernameLabel = GHFTitleLabel(textAligment: .left, fontSize: 34)
+    let nameLabel = GHFSecondaryTitleLabel(fontSize: 18)
+    let locationImageView = UIImageView()
+    let locationLabel = GHFSecondaryTitleLabel(fontSize: 18)
+    let bioLabel = GHFBodyLabel(textAligment: .left)
+    
+    var user: User!
+    
+    //MARK: - Inits
+    init(user: User) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        addSubviews()
+        layoutUI()
+    }
+    
+    //MARK: - Methods
+    //Configure UI elements
+    private func configure() {
+        avatarImageView.downloadImage(from: user.avatarUrl)
+        usernameLabel.text = user.login
+        nameLabel.text = user.name ?? "Имя не указано"
+        locationLabel.text = user.location ?? "Локация не указана"
+        bioLabel.text = user.bio ?? "..."
+        bioLabel.numberOfLines = 3
+        
+        locationImageView.image = UIImage(systemName: "mappin.circle.fill")
+        locationImageView.tintColor = .secondaryLabel
+    }
+    
+    //Add subviews
+    private func addSubviews() {
+        view.addSubview(avatarImageView)
+        view.addSubview(usernameLabel)
+        view.addSubview(nameLabel)
+        view.addSubview(locationImageView)
+        view.addSubview(locationLabel)
+        view.addSubview(bioLabel)
+    }
+    
+    //Layouts
+    private func layoutUI() {
+        locationImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            avatarImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 90),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 90),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.topAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 12),
+            usernameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 38),
+            
+            nameLabel.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor, constant: 5),
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 12),
+            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            nameLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            locationImageView.bottomAnchor.constraint(equalTo: avatarImageView.bottomAnchor),
+            locationImageView.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 12),
+            locationImageView.widthAnchor.constraint(equalToConstant: 20),
+            locationImageView.heightAnchor.constraint(equalToConstant: 20),
+            
+            locationLabel.centerYAnchor.constraint(equalTo: locationImageView.centerYAnchor),
+            locationLabel.leadingAnchor.constraint(equalTo: locationImageView.trailingAnchor, constant: 5),
+            locationLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            locationLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            bioLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            bioLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bioLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            bioLabel.heightAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+}
